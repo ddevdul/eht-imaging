f2ee8b529d11aae03241fa80e475e2cc1a38d3ae
ddevdul <ddulin96@gmail.com>
2022-01-29 11:31:55 -0500

    Cleaning up the constructor. I added a private method for tkey and also removed a loop that wasn't needed. This new implementation should improve run time. Also updating this to run on python 3.10

--- ehtim/array.py
+++ ehtim/array.py
@@ -1,78 +1,65 @@
-# array.py
-# a interferometric telescope array class
-#
-#    Copyright (C) 2018 Andrew Chael
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU General Public License as published by
-#    the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU General Public License for more details.
-#
-#    You should have received a copy of the GNU General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+A interferometric telescope array class
 
-from __future__ import division
-from __future__ import print_function
+Copyright (C) 2022 Andrew Chael
 
-from builtins import str
-from builtins import range
-from builtins import object
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-import numpy as np
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-import ehtim.observing.obs_simulate as simobs
-import ehtim.io.save
-import ehtim.io.load
-import ehtim.const_def as ehc
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
-###################################################################################################
-# Array object
-###################################################################################################
+import numpy
+from ehtim.observing import obs_simulate  # as simobs
+from ehtim.io import save, load # ehtim.io.load ehtim.io.save
+from  ehtim import const_def # as ehc
 
 
-class Array(object):
+class Array:
 
-    """A VLBI array of telescopes with site locations, SEFDs, and other data.
+    """
+    A VLBI array of telescopes with site locations, SEFDs, and other data.
 
        Attributes:
            tarr (numpy.recarray): The array of telescope data with datatype DTARR
            tkey (dict): A dictionary of rows in the tarr for each site name
            ephem (dict): A dictionary of TLEs for each space antenna,
-                         Space antennas have x=y=z=0 in the tarr
+                        Space antennas have x=y=z=0 in the tarr
     """
 
-    def __init__(self, tarr, ephem={}):
+    # fix the types that go in the dictionary
+    def __init__(self, tarr: numpy.recarray, ephem: dict[str, None]):
         self.tarr = tarr
         self.ephem = ephem
-
-        # check to see if ephemeris is correct
-        for line in self.tarr:
-            if np.any(np.isnan([line['x'], line['y'], line['z']])):
+        self.tkey = self._check_ephemeris() 
+    
+    def _check_ephemeris(self) -> dict[str, None]:
+        array_indicies = dict()
+        for i, line in enumerate(self.tarr):
+            if numpy.any(numpy.isnan([line['x'], line['y'], line['z']])):
                 sitename = str(line['site'])
                 try:
-                    elen = len(ephem[sitename])
+                    elen = len(self.ephem[sitename])
                 except NameError:
-                    raise Exception('no ephemeris for site %s !' % sitename)
+                    raise Exception(f'No ephemeris for site {sitename} !')
                 if elen != 3:
+                    raise Exception('Wrong ephemeris format for site {sitename} !')
+            array_indicies[self.tarr[i]['site']] = i
 
-                    raise Exception('wrong ephemeris format for site %s !' % sitename)
-
-        # Dictionary of array indices for site names
-        self.tkey = {self.tarr[i]['site']: i for i in range(len(self.tarr))}
-
-    def listbls(self):
-        """List all baselines.
-
-           Args:
-           Returns:
-                numpy.array : array of baselines
+    def listbls(self) -> numpy.array:
         """
-
+        List all baselines.
+        Returns:
+            numpy.array : array of baselines
+        """
         bls = []
         for i1 in sorted(self.tarr['site']):
             for i2 in sorted(self.tarr['site']):
