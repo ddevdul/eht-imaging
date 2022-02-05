6d1a707cb64cd9edcf0fb26cc36ece92737716c3
ddevdul <ddulin96@gmail.com>
2022-02-05 17:32:02 -0500

    Fixing imports and adding main

--- examples/example_pipeline.py
+++ examples/example_pipeline.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python2
-
-import ehtim as eh
+import sys
+sys.path.extend(["../"])
+import ehtim
 import ruamel.yaml as yaml
 
-input = "M87/er4v2/data/lo/hops_3601_M87.LL+netcal.uvfits"
+def main():
+    input = "M87/er4v2/data/lo/hops_3601_M87.LL+netcal.uvfits"
+    print("----------------------------------------------------------------")
+    print("Use the ehtim pipeline interface as a pipeline constructor")
+    with open("example_pipeline.yaml", 'r') as f:
+        pipeline = ehtim.Pipeline(yaml.load(f))
+    obs = pipeline.apply(input).data
+    print("----------------------------------------------------------------")
+    print("Use the ehtim pipeline interface with method-chaining notation")
+    obs = ehtim.Pipeline(input) \
+        .load() \
+        .average(minlen=500, sec=300.0) \
+        .flag(uv_min=0.1e9) \
+        .flag(site='SR') \
+        .reorder() \
+        .data
 
-print("----------------------------------------------------------------")
-print("Use the ehtim pipeline interface as a pipeline constructor")
 
-with open("example_pipeline.yaml", 'r') as f:
-    pipeline = eh.Pipeline(yaml.load(f))
-
-obs = pipeline.apply(input).data
-
-print("----------------------------------------------------------------")
-print("Use the ehtim pipeline interface with method-chaining notation")
-
-obs = eh.Pipeline(input) \
-    .load() \
-    .average(minlen=500, sec=300.0) \
-    .flag(uv_min=0.1e9) \
-    .flag(site='SR') \
-    .reorder() \
-    .data
+if __name__ == "__main__":
+    main()
